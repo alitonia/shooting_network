823bd3b621d6835fabd0fdab4c3146ff352a9b21
Zen-net <allyofjustice1@gmail.com>
2021-03-17 02:14:46 +0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/network_client.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zen-net/work/network_client/tcp_client.cpp" "/home/zen-net/work/network_client/cmake-build-debug/CMakeFiles/network_client.dir/tcp_client.cpp.o"
+  "/home/zen-net/work/network_client/tcp_server.cpp" "/home/zen-net/work/network_client/cmake-build-debug/CMakeFiles/network_client.dir/tcp_server.cpp.o"
+  "/home/zen-net/work/network_client/udp_client.cpp" "/home/zen-net/work/network_client/cmake-build-debug/CMakeFiles/network_client.dir/udp_client.cpp.o"
+  "/home/zen-net/work/network_client/udp_server.cpp" "/home/zen-net/work/network_client/cmake-build-debug/CMakeFiles/network_client.dir/udp_server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/network_client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/network_client.dir/tcp_client.cpp.o"
+  "CMakeFiles/network_client.dir/tcp_server.cpp.o"
+  "CMakeFiles/network_client.dir/udp_client.cpp.o"
+  "CMakeFiles/network_client.dir/udp_server.cpp.o"
+  "network_client"
+  "network_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/network_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
